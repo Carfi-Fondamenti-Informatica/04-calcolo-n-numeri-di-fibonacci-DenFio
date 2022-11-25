043957bbb90774c0c0ac6ddbaa75f2649617e6c1
Dennis Fiorenza <115047044+DenFio@users.noreply.github.com>
2022-11-25 10:24:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
 int n=0, a=1, b=1, c=0;
 cin >> n;
 If (n>=2) {
-cout << a << endl << b << endl ;
+cout << a << endl << b << endl;
 for (int i=0; i <= n; i++){
 c=a+b;
 a=b;
@@ -14,7 +14,7 @@ cout << c << endl;
 
   }
 }
-Else {
+else {
 cout << "errore" << endl;
 }
    return 0;
